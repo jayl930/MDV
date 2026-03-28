b5c2fcc3e590a6056c19361a4cee5cc0aaef8011
jayl930 <jyl11@illinois.edu>
2026-03-29 01:32:21 +0900

    Initial Commit

---
+++ MDV/MDVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDVApp.swift
+//  MDV
+//
+//  Created by Jay Lee on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct MDVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
